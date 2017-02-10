5d853fff78988656ca6a4e41a26fb319e87f4c5d
panjiyar <panjiyar.suchit@gmail.com>
2017-02-09 19:24:31 -0800

    Finished echo tests

--- echo_handler_test.cc
+++ echo_handler_test.cc
@@ -1,2 +1,18 @@
 #include "gtest/gtest.h"
 #include "echo_handler.hpp"
+#include "request.hpp"
+#include "reply.hpp"
+
+
+TEST(echo_handle_, handle_request_)
+{
+    http::server::echo_handler echo_handler_;
+    http::server::request request_;
+    request_.content[0] = 'A';
+    request_.content[0] = 'B';
+    request_.content[0] = 'C';
+    request_.length = 3;
+    http::server::reply reply_;
+    EXPECT_TRUE(echo_handler_.handle_request(request_, reply_));
+}
+
--- request_handler_test.cc
+++ request_handler_test.cc
@@ -5,8 +5,22 @@
 #include <string>
 
 
-TEST(request_handle_constructor_and_rh_, URL)
+TEST(request_handle_, URL)
 {
     std::string echo_str = "/echo", empty = "";
     EXPECT_NO_THROW(http::server::request_handler::generateHandler(echo_str, empty));
+}
+
+
+TEST(request_handle_, static_)
+{
+    std::string static_str = "/static", root = "/test";
+    EXPECT_NO_THROW(http::server::request_handler::generateHandler(static_str, root));
+}
+
+
+TEST(request_handle_, null_)
+{
+    std::string rnd_str = "LOL", empty = "";
+    EXPECT_EQ(http::server::request_handler::generateHandler(rnd_str, empty),nullptr);
 }
