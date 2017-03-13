ece73d169c38df1c233a9fed7af053bb93bc7968
panjiyar <panjiyar.suchit@gmail.com>
2017-03-12 21:47:34 -0700

    Added correct server tests

--- server_test.cc
+++ server_test.cc
@@ -5,8 +5,15 @@
 
 std::unordered_map<std::string, std::string> test_map;
 
+NginxConfig retGoodConfig()
+{
+    NginxConfigParser parser;
+    NginxConfig test_config;
+    parser.Parse("config_file", &test_config);
+    return test_config;
+}
 
-TEST(ServerTest, BadAddrAndEmptyConfig) 
+TEST(ServerTest, BadAddrAndEmptyConfig)
 {
   NginxConfig empty_config;
 
@@ -25,14 +32,16 @@ TEST(ServerTest, GoodAddrAndEmptyConfig)
   EXPECT_ANY_THROW(http::server::server test_server("127.0.0.1", empty_config));
 }
 
-TEST(ServerTest, GoodConfigs) 
+TEST(ServerTest, NotGoodConfigs)
 {
-  NginxConfigParser parser;
-  NginxConfig test_config;
-  parser.Parse("config_file", &test_config);
-
+    NginxConfig test_config = retGoodConfig();
   EXPECT_ANY_THROW(http::server::server test_server("127.0.0.1", test_config));
   EXPECT_ANY_THROW(http::server::server test_server("127.0.0.1", test_config));
 }
 
-
+TEST(ServerTest, GoodConfigs)
+{
+    NginxConfig config = retGoodConfig();
+    http::server::server test("0.0.0.0", config);
+    test.kill();
+}
