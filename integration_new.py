88cc2d41ccef206cc24deebdade2b17ebb7179d9
Roi B <rbarank@g.ucla.edu>
2017-03-30 16:02:19 -0700

    Update integration_new.py Split each type of test into it's own function. Started unifying style because this file is a merge of several integration tests written by different people.

--- integration_new.py
+++ integration_new.py
@@ -12,7 +12,7 @@ PROXY_CONFIG_NAME = "config_file"
 def threadExec():
 	subprocess.call(["./webserver", "config_file"])
 
-def multi_thread_check():
+def multithread_test():
 	req = ["GET /echo HTTP/1.1", "Host: localhost:3000", "Connection: keep-alive", "Upgrade-Insecure-Requests: 1"]
 	req += ["User-Agent: telnet", "Accept: */*"]
 	req = "\r\n".join(req)
@@ -42,7 +42,7 @@ def multi_thread_check():
 def proxyThreadExec():
     subprocess.call(["./webserver", PROXY_CONFIG_NAME])
 
-def proxyTest():
+def proxy_test():
     print "Proxy integration test:"
     proxyProcess = Process(target=proxyThreadExec)
     proxyProcess.start()
@@ -64,6 +64,43 @@ def proxyTest():
     proxyProcess.terminate()
     print "Proxy integration test passed!"
 
+def echo_test():
+        req = requests.get('http://localhost:3000/echo/')
+
+        if req.status_code != 200:
+                print "Error: Wrong Status Code."
+                exit(1)
+        else:
+                print "Status Code Correct: 200"
+                
+        if req.headers['content-type'] != 'text/plain':
+                print "Incorrect content-type"
+                exit(2)
+        else:
+                print "Content Type Correct: text/plain"
+
+def proxy_redirect_test():
+        server = subprocess.Popen(["./webserver", "config_proxy302"])
+        sleep(2)
+        
+        direct = subprocess.call(["curl", "-s", "http://ucla.edu", "-o", "result_direct.txt"])
+        proxy = subprocess.call(["curl", "-s", "http://localhost:8082/", "-o", "result_proxy.txt"])
+        sleep(10)
+        
+        diffOutcome = subprocess.Popen(["diff", "-u", "result_direct.txt", "result_proxy.txt"], stdout = subprocess.PIPE)
+        diff = diffOutcome.stdout.read().decode("utf-8")
+        
+        server.kill()
+        
+        print "length of original result is "
+        print os.stat("result_direct.txt").st_size
+        print "length of redirect proxy result is "
+        print os.stat("result_proxy.txt").st_size
+        
+        os.remove("result_direct.txt")
+        os.remove("result_proxy.txt")
+
+
 
 ### BEGIN TESTS
 serverProcess = Process(target=threadExec)
@@ -72,49 +109,25 @@ sleep(5)
 
 
 # ECHO TEST
-req = requests.get('http://localhost:3000/echo/')
+print "STARTING ECHO TEST ------------------------------"
+echo_test()
+print "ECHO TEST PASSED ------------------------------"
 
-if req.status_code != 200:
-	print "Error: Wrong Status Code."
-	exit(1)
-else:
-	print "Status Code Correct: 200"
-
-if req.headers['content-type'] != 'text/plain':
-	print "Incorrect content-type"
-	exit(2)
-else:
-	print "Content Type Correct: text/plain"
 
 # MULTITHREAD TEST
-multi_thread_check()
-
+print "STARTING MULTITHREAD TEST ------------------------------"
+multithread_test()
+print "MULTITHREAD TEST PASSED ------------------------------"
 
 # PROXY TEST
-proxyTest()
+print "STARTING PROXY TEST ------------------------------"
+proxy_test()
+print "PROXY TEST PASSED ------------------------------"
 
-server = subprocess.Popen(["./webserver", "config_proxy302"])
-sleep(2)
-
-direct = subprocess.call(["curl", "-s", "http://ucla.edu", "-o", "result_direct.txt"])
-proxy = subprocess.call(["curl", "-s", "http://localhost:8082/", "-o", "result_proxy.txt"])
-sleep(10)
-
-diffOutcome = subprocess.Popen(["diff", "-u", "result_direct.txt", "result_proxy.txt"], stdout = subprocess.PIPE)
-diff = diffOutcome.stdout.read().decode("utf-8")
-
-server.kill()
-
-
-print "length of original result is "
-print os.stat("result_direct.txt").st_size
-print "length of redirect proxy result is "
-print os.stat("result_proxy.txt").st_size
-
-os.remove("result_direct.txt")
-os.remove("result_proxy.txt")
-
-print "redirect test passed!"
+# PROXY REDIRECT TEST
+print "STARTING PROXY REDIRECT TEST ------------------------------"
+proxy_redirect_test()
+print "PROXY REDIRECT TEST PASSED ------------------------------"
 
 
 print "Integration Test Passed!"
